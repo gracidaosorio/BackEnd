0320ea30c1ae6a1ead7fbc85dfa2d2e337cdf7a8
gracidaosorio <l17350293@ittux.edu.mx>
2021-01-18 20:51:02 -0600

    Instalación de Express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo");
+const express = require('express');
+
+//Crear Servidor Express
+const app = express();
+
+//Rutas
+app.get('/',(req,res) => {
+    console.log('Se recibio una peticion en /');
+    res.json({
+        ok: true,
+        msg: "Recibido",
+    });
+});
+
+//Configuración del puerto
+app.list(4000,() => {
+    console.log(`BackEnd corriendo en el puerto ${4000}`);
+})
